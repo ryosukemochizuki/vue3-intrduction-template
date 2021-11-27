40c76bf3334a7ea262d3e8659a87d9e29fbbfaee
ryosukemochizuki <ryosukemochizuki.mochi@gmail.com>
2021-11-27 09:52:52 +0900

    :tada: first commit

--- README.md
+++ README.md
@@ -1,24 +1,3 @@
-# hello-vue3
+# Vue3-introduction-template
 
-## Project setup
-```
-npm install
-```
-
-### Compiles and hot-reloads for development
-```
-npm run serve
-```
-
-### Compiles and minifies for production
-```
-npm run build
-```
-
-### Lints and fixes files
-```
-npm run lint
-```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+これは、Vue2 を実装したことがある人を想定しています。
--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,33 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <h1>{{ counter }}</h1>
+  <h2>{{ twiceNum }}</h2>
+  <button @click="plusButton"></button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import { computed, ref } from "@vue/reactivity";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  setup() {
+    const counter = ref(0);
+    const plusButton = () => {
+      counter.value++;
+    };
+    const twiceNum = computed(() => {
+      return counter.value * 2;
+    });
+    return {
+      counter,
+      plusButton,
+      twiceNum,
+    };
+  },
+  // computed: {
+  //   twiceNum: function () {
+  //     return this.counter * 2;
+  //   },
+  // },
+};
 </script>
 
 <style>
